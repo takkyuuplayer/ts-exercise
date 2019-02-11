37f38fb9dba52c562817c9415ee12a1d340af813
takkyuuplayer <takkyuuplayer@gmail.com>
2019-02-10 23:51:36 -0500

    Add iterator, symbol

---
+++ test/iterators.spec.ts
@@ -0,0 +1,38 @@
+describe("Iterables", () => {
+  it("iterates whatever implements Symbol.iterator", () => {
+    const iterable = {
+      *[Symbol.iterator]() {
+        for (let index = 0; index < 5; index++) {
+          yield index * index;
+        }
+      },
+    };
+    const iterated = [];
+    for (const val of iterable) {
+      iterated.push(val);
+    }
+    expect(iterated).toStrictEqual([0, 1, 4, 9, 16]);
+  });
+  describe("for ... of", () => {
+    it("iterates over values", () => {
+      const arr = [1, 4, 9];
+      let sum = 0;
+      for (const val of arr) {
+        sum += val;
+      }
+      expect(sum).toBe(14);
+    });
+  });
+  describe("for ... in", () => {
+    it("iterates over keys", () => {
+      const arr = [1, 4, 9];
+      let sum = "";
+      for (const key in arr) {
+        if (arr.hasOwnProperty(key)) {
+          sum += key;
+        }
+      }
+      expect(sum).toBe("012");
+    });
+  });
+});
---
+++ test/symbol.spec.ts
@@ -0,0 +1,12 @@
+describe("symbol", () => {
+  it("is immutable and unique", () => {
+    expect(Symbol("key")).not.toEqual(Symbol("key"));
+  });
+  it("can be used as keys for object properties", () => {
+    const sym = Symbol();
+    const obj = {
+      [sym]: "value",
+    };
+    expect(obj[sym]).toBe("value");
+  });
+});
