ba7bdd96cfc199302b424f281f7b59618dc879b6
takkyuuplayer <takkyuuplayer@gmail.com>
2020-01-03 21:16:56 -0500

    Add some

---
+++ test/path.spec.ts
@@ -0,0 +1,9 @@
+import path from 'path'
+describe('path', () => {
+    describe('resolve', () => {
+        it('returns current working directory', () => {
+            expect(path.resolve()).toBe(process.cwd())
+            expect(path.resolve().endsWith('ts-exercise')).toBeTruthy()
+        });
+    });
+});
