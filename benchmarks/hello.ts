013a0c2ebc8e160f8b91174b56fc47a82f74f83a
takkyuuplayer <takkyuuplayer@gmail.com>
2020-02-18 00:42:20 -0500

    Add benchmarker

---
+++ benchmarks/hello.ts
@@ -0,0 +1,19 @@
+import Benchmark from "benchmark";
+
+const suite = new Benchmark.Suite();
+suite
+  .add("RegExp#test", function() {
+    /o/.test("Hello World!");
+  })
+  .add("String#indexOf", function() {
+    "Hello World!".indexOf("o") > -1;
+  })
+  // add listeners
+  .on("cycle", function(event: any) {
+    console.log(String(event.target));
+  })
+  .on("complete", function(this: any) {
+    console.log("Fastest is " + this.filter("fastest").map("name"));
+  })
+  // run async
+  .run({ async: true });
