22c6faf3c62d3d21ef73940cc27c1f9ace64bd46
takkyuuplayer <takkyuuplayer@gmail.com>
2020-02-18 01:23:29 -0500

    Add loggers

--- benchmarks/logger.ts
+++ benchmarks/logger.ts
@@ -1,9 +1,18 @@
 import winston from "winston";
 import tmp from "tmp";
 import Benchmark from "benchmark";
+import bunyan from "bunyan";
 
 const winstonLogger = winston.createLogger({
-  transports: [new winston.transports.Console()]
+  transports: [new winston.transports.File({ filename: tmp.fileSync().name })]
+});
+const bunyanLogger = bunyan.createLogger({
+  name: "bunyan",
+  streams: [
+    {
+      path: tmp.fileSync().name
+    }
+  ]
 });
 
 const suite = new Benchmark.Suite();
@@ -11,8 +20,8 @@ suite
   .add("winston.info", function() {
     winstonLogger.info("Hello World");
   })
-  .add("console.info", function() {
-    console.info("Hello World");
+  .add("bunyan.info", function() {
+    bunyanLogger.info("Hello World");
   })
   .on("cycle", function(event: any) {
     console.log(String(event.target));
