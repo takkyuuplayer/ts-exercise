cbad1a58dee68b09f6665b0031ec8065d3406f87
takkyuuplayer <takkyuuplayer@gmail.com>
2020-02-23 18:03:38 -0500

    one-to-one relation with one-way

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Photo } from "./entity/Photo";
+import { PhotoMetadata } from "./entity/PhotoMetadata";
 
 createConnection({
   type: "sqlite",
@@ -10,15 +11,36 @@ createConnection({
   logging: true
 })
   .then(async connection => {
+    // create a photo
+    let photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.filename = "photo-with-bears.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
+
+    // create a photo metadata
+    let metadata = new PhotoMetadata();
+    metadata.height = 640;
+    metadata.width = 480;
+    metadata.compressed = true;
+    metadata.comment = "cybershoot";
+    metadata.orientation = "portait";
+    metadata.photo = photo; // this way we connect them
+
+    // get entity repositories
     let photoRepository = connection.getRepository(Photo);
+    let metadataRepository = connection.getRepository(PhotoMetadata);
 
-    let photoToRemove = await photoRepository.findOne(1);
-    await photoRepository.remove(photoToRemove!);
+    // first we should save a photo
+    await photoRepository.save(photo);
 
-    let [allPhotos, photosCount] = await photoRepository.findAndCount({
-      isPublished: true
-    });
-    console.log("All photos: ", allPhotos);
-    console.log("Photos count: ", photosCount);
+    // photo is saved. Now we need to save a photo metadata
+    await metadataRepository.save(metadata);
+
+    // done
+    console.log(
+      "Metadata is saved, and relation between metadata and photo is created in the database too"
+    );
   })
   .catch(error => console.log(error));
