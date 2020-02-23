913ca40d07fa2f862143be92f215841d5f661b27
takkyuuplayer <takkyuuplayer@gmail.com>
2020-02-23 17:34:27 -0500

    Using repository

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entity/User";
 import { Photo } from "./entity/Photo";
 
 createConnection({
@@ -11,18 +10,11 @@ createConnection({
   logging: true
 })
   .then(async connection => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
+    let photoRepository = connection.getRepository(Photo);
+    let [allPhotos, photosCount] = await photoRepository.findAndCount({
+      isPublished: true
+    });
+    console.log("All photos: ", allPhotos);
+    console.log("Photos count: ", photosCount);
   })
   .catch(error => console.log(error));
