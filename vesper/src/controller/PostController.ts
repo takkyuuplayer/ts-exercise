a31fc040733590008f65360602fd849a21479696
takkyuuplayer <takkyuuplayer@gmail.com>
2020-02-23 21:03:20 -0500

    vesper getting started

---
+++ vesper/src/controller/PostController.ts
@@ -0,0 +1,34 @@
+import { Controller, Query, Mutation } from "vesper";
+import { EntityManager } from "typeorm";
+import { Post } from "../entity/Post";
+
+@Controller()
+export class PostController {
+  constructor(private entityManager: EntityManager) {}
+
+  // serves "posts: [Post]" requests
+  @Query()
+  posts() {
+    return this.entityManager.find(Post);
+  }
+
+  // serves "post(id: Int): Post" requests
+  @Query()
+  post({ id }) {
+    return this.entityManager.findOne(Post, id);
+  }
+
+  // serves "postSave(id: Int, title: String, text: String): Post" requests
+  @Mutation()
+  postSave(args) {
+    const post = this.entityManager.create(Post, args);
+    return this.entityManager.save(Post, post);
+  }
+
+  // serves "postDelete(id: Int): Boolean" requests
+  @Mutation()
+  async postDelete({ id }) {
+    await this.entityManager.remove(Post, { id: id });
+    return true;
+  }
+}
