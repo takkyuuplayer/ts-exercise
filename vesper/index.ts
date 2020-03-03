f7f833f5da4156a0a41901238859dd426f4aa240
takkyuuplayer <takkyuuplayer@gmail.com>
2020-03-03 00:18:43 -0500

    tmp

---
+++ vesper/index.ts
@@ -0,0 +1,19 @@
+import { bootstrap } from "vesper";
+import { PostController } from "./controller/PostController";
+import { Post } from "./entity/Post";
+
+bootstrap({
+  port: 3000,
+  controllers: [PostController],
+  entities: [Post],
+  schemas: [__dirname + "/schema/**/*.graphql"]
+})
+  .then(() => {
+    console.log(
+      "Your app is up and running on http://localhost:3000. " +
+        "You can use playground in development mode on http://localhost:3000/playground"
+    );
+  })
+  .catch(error => {
+    console.error(error.stack ? error.stack : error);
+  });
