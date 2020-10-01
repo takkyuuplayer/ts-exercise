4a5a9c2869382d27af4901bb39e2aefb3939e19a
takkyuuplayer <takkyuuplayer@gmail.com>
2020-10-01 17:21:30 +0900

    Fix lint

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,21 @@ createConnection({
   database: "db.sqlite3",
   entities: ["src/entity/**/*.ts"],
   synchronize: true,
-  logging: true
+  logging: true,
 })
-  .then(async connection => {
-    let photo = Photo.fake({ name: "Test" });
-    let metadata = PhotoMetadata.fake();
+  .then(async (connection) => {
+    const photo = Photo.fake({ name: "Test" });
+    const metadata = PhotoMetadata.fake();
     photo.metadata = metadata; // this way we connect them
 
-    let photoRepository = connection.getRepository(Photo);
+    const photoRepository = connection.getRepository(Photo);
 
     // saving a photo also save the metadata
     await photoRepository.save(photo);
 
     console.log("Photo is saved, photo metadata is saved too.");
 
-    let photos = await connection
+    const photos = await connection
       .getRepository(Photo)
       .createQueryBuilder("photo")
       .innerJoinAndSelect("photo.metadata", "metadata")
@@ -30,4 +30,4 @@ createConnection({
 
     console.log(photos);
   })
-  .catch(error => console.log(error));
+  .catch((error) => console.log(error));
