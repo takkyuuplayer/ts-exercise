85486962b7130bcbb3ad2ab5410cf8ed6aac0fe8
takkyuuplayer <takkyuuplayer@gmail.com>
2023-03-14 22:29:40 +0900

    Pre-Assets, Metadata, and CSS

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -1,13 +1,21 @@
-import Link from 'next/link';
-import React from 'react';
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Layout from "../../components/layout";
+
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
     </>
   );
 }
