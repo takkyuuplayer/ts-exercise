355e8fa727261a6b03f941fbbef02d96945605b7
takkyuuplayer <takkyuuplayer@gmail.com>
2024-08-05 23:49:14 +0900

    fix

--- test/type-inference.spec.ts
+++ test/type-inference.spec.ts
@@ -1,18 +1,15 @@
+// eslint-disable @typescript-eslint/no-unused-vars
 describe("type inference", () => {
   it("infere type of variable", () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const x = 3;
     expect(typeof x).toBe("number");
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const y = "string";
     expect(typeof y).toBe("string");
   });
   it("uses union type", () => {
     const x = [0, 1, null];
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const y: typeof x = [null];
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const z: typeof x = [0];
     // const w: typeof x = ["string"]; // forbidden
   });
