b4ae074cd4f7e0d9a027d67bd7cc11bd3bd2f1de
takkyuuplayer <takkyuuplayer@gmail.com>
2025-08-18 21:46:03 +0900

    fix

--- test/socket.io/server.spec.ts
+++ test/socket.io/server.spec.ts
@@ -1,6 +1,6 @@
-import express from "express";
 import { createServer } from "node:http";
-import { AddressInfo } from "node:net";
+import type { AddressInfo } from "node:net";
+import express from "express";
 import { Server } from "socket.io";
 import { io } from "socket.io-client";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
