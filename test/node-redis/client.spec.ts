f9464577bcf86719fd6bc1f0436102415f87a7ae
takkyuuplayer <takkyuuplayer@gmail.com>
2025-09-09 00:29:07 +0900

    update redis

--- test/node-redis/client.spec.ts
+++ test/node-redis/client.spec.ts
@@ -1,28 +1,42 @@
 import { createClient, defineScript } from "redis";
-import { describe, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 
 describe("defineScript", async () => {
-  it("should work", async () => {
-    const client = createClient({
-      url: "redis://localhost:56379",
-      scripts: {
-        mincr: defineScript({
-          NUMBER_OF_KEYS: 2,
-          SCRIPT:
-            "return {" +
-            'redis.pcall("INCRBY", KEYS[1], ARGV[1]),' +
-            'redis.pcall("INCRBY", KEYS[2], ARGV[1])' +
-            "}",
-          transformArguments(key1, key2, increment) {
-            return [key1, key2, increment.toString()];
-          },
-        }),
-      },
-    });
+  const client = createClient({
+    url: "redis://localhost:56379",
+    scripts: {
+      mincr: defineScript({
+        NUMBER_OF_KEYS: 2,
+        SCRIPT:
+          "return {" +
+          'redis.pcall("INCRBY", KEYS[1], ARGV[1]),' +
+          'redis.pcall("INCRBY", KEYS[2], ARGV[1])' +
+          "}",
+        parseCommand(parser, key1, key2, increment) {
+          parser.pushKey(key1);
+          parser.pushKey(key2);
+          parser.push(increment.toString());
+        },
+        transformReply(reply: [number, number]) {
+          return reply;
+        },
+      }),
+    },
+  });
 
+  beforeAll(async () => {
     await client.connect();
+    await client.flushAll();
+  });
+
+  it("should work", async () => {
+    await client.connect();
+    await client.flushAll();
 
     await client.set("mykey", "5");
-    console.log(await client.mincr("mykey", "myotherkey", 10));
+    const [mykey, myotherkey] = await client.mincr("mykey", "myotherkey", 10);
+
+    expect(mykey).toBe(15);
+    expect(myotherkey).toBe(10);
   });
 });
